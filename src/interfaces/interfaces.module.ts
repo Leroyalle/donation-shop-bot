0482d91153dbec288da75bf29b6077086695c3ce
Leroyalle <nikolajmelonov@yandex.ru>
2025-11-11 01:22:02 +0300

    fix(payment-interface): import payment interface module to interfaces and import necessary modules

--- src/interfaces/interfaces.module.ts
+++ src/interfaces/interfaces.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TelegramModule } from './telegram/telegram.module';
+import { PaymentInterfaceModule } from './payment/payment.interface.module';
 
 @Module({
-  imports: [TelegramModule],
+  imports: [TelegramModule, PaymentInterfaceModule],
 })
 export class InterfacesModule {}
--- src/interfaces/payment/payment.interface.module.ts
+++ src/interfaces/payment/payment.interface.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentWebhookService } from './services/payment-webhook.service';
+import { OrderModule } from 'src/domain/order/order.module';
+import { CartModule } from 'src/domain/cart/cart.module';
+import { CartItemModule } from 'src/domain/cart-item/cart-item.module';
 
 @Module({
+  imports: [OrderModule, CartModule, CartItemModule],
   controllers: [PaymentController],
   providers: [PaymentWebhookService],
 })
