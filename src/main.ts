937fa8baf5e694edc02f5245f87036269938a709
Leroyalle <nikolajmelonov@yandex.ru>
2025-11-13 19:58:09 +0300

    feat(app): enable cors for allowed url

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +13,12 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.enableCors({
+    origin: configService.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['Set-Cookie'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
